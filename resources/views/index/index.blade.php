5662a862c5faa59c2d95de5e32d5e17fed658ed3
hechaqu <hechaqu123@163.com>
2016-08-31 21:47:25 +0800

    index

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <link rel="stylesheet" href="index.css" type="text/css" />
+</head>
+<body>
+<div id="nav">
+    nav
+</div>
+<div id="left">
+    left
+</div>
+<div id="right">
+    right
+</div>
+<div id="clear">
+</div>
+<div id="footer">
+    footer
+</div>
+</body>
+</html>
